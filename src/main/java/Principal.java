b7f885d343a37062509fae5a4e667bc694eb3236
Otax-png <taxturnil201@gmail.com>
2021-06-26 14:12:42 -0600

    Agregando Graficos Principales

--- src/main/java/Principal.java
+++ src/main/java/Principal.java
@@ -1,7 +1,9 @@
 package Backend;
 
+import Frontend.VentanaPrincipal;
+
 public class Principal {
     public static void main(String[] args) {
-
+        VentanaPrincipal t1 = new VentanaPrincipal();
     }
 }
